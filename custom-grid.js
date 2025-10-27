7d0197d623313da856630967ba349cafb3bb7558
flowerfulpowerful <tacticalvoidd@gmail.com>
2025-10-27 14:08:52 -0500

    Update custom-grid.js

--- custom-grid.js
+++ custom-grid.js
@@ -671,7 +671,6 @@ class CustomGridManager {
                     (row === 0 && col === 4) ||
                     (row === 0 && col === 5) ||
                     (row === 0 && col === 6) ||
-                    (row === 0 && col === 7) ||
                     (row === 0 && col === 12) ||
                     
                     (row === 1 && col === 0) ||
@@ -680,20 +679,17 @@ class CustomGridManager {
                     (row === 1 && col === 3) ||
                     (row === 1 && col === 4) ||
                     (row === 1 && col === 5) ||
-                    (row === 1 && col === 6) ||
 
                     (row === 2 && col === 0) ||
                     (row === 2 && col === 1) ||
                     (row === 2 && col === 2) ||
                     (row === 2 && col === 3) ||
                     (row === 2 && col === 4) ||
-                    (row === 2 && col === 5) ||
  
                     (row === 3 && col === 0) ||
                     (row === 3 && col === 1) ||
                     (row === 3 && col === 2) ||
                     (row === 3 && col === 3) ||
-                    (row === 3 && col === 4) ||
 
                     (row === 4 && col === 0) ||
                     (row === 4 && col === 12) ||
